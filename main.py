12c50442ee5b1ae690dbcfe38e2a9be671ace50a
ekhossejn <45770055+ekhossejn@users.noreply.github.com>
2019-12-08 23:07:21 +0300

    Main without design

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from kivy.app import App
-from kivy.uix.button import Button
+from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.config import Config
 from kivy.lang import Builder
-from kivy.properties import NumericProperty, StringProperty
 from kivy.clock import Clock
-from kivy.uix.widget import Widget
-
+from kivy.app import App
 
 Config.set("graphics", "width", 390) 
 Config.set("graphics", "heigth", 600)
@@ -27,8 +26,8 @@ class Time(Widget):
 class Num(Widget):
     num = NumericProperty(1)
 
-
 class WindowManager(ScreenManager):
+
     time = Time()
     num = Num()
     
@@ -47,85 +46,9 @@ class WindowManager(ScreenManager):
         self.num.num = 1
         self.event.cancel()
 
-kv = Builder.load_string('''
-<WindowManager>:
-    ScreenOne:
-        id: 1
-    ScreenTwo:
-        id: 2
-<ScreenOne>:
-    name : "1"
-    FloatLayout:
-        orientation: 'vertical'
-        canvas:
-            Rectangle:
-                source: 'C:/Users/79263/Downloads/Старт (3).png'
-                size: 390, 600
-                pos: self.pos
-        Button:
-            text: "НАЧНЁМ"
-            font_size : '30sp'
-            color: 3, 3, 3, 1
-            pos: 80, 285
-            size_hint: .6, .1
-            background_color : 0, 0, 0, 0
-            background_normal : ''
-            back_color: 1, 0, 1, 1
-            border_radius : 18
-            bold: True
-            canvas.before:
-                Color:
-                    rgba: self.back_color
-                Line:
-                    rounded_rectangle: (self.pos[0], self.pos[1], self.size[0], self.size[1], self.border_radius)
-                    width : 1.2
-            on_release:
-                root.manager.transition.direction = "down"
-                root.manager.transition.duration = 0
-                root.manager.current = "2"
-                root.manager.count_down()
-<ScreenTwo>
-    name : "2"
-    FloatLayout:
-        orientation: 'vertical'
-        canvas:
-            Rectangle:
-                source: 'C:/Users/79263/Downloads/ПОЕХАЛИ ! (4).png'
-                size: 390, 600
-                pos: self.pos
-        Button:
-            text: "Закончим"
-            font_size : '30sp'
-            color: 3, 3, 3, 1
-            pos: 85, 90
-            size_hint: .6, 0.08
-            background_color : 0, 0, 0, 0
-            background_normal : ''
-            back_color: 1, 0, 1, 1
-            border_radius : 18
-            bold: True
-            canvas.before:
-                Color:
-                    rgba: self.back_color
-                Line:
-                    rounded_rectangle: (self.pos[0], self.pos[1], self.size[0], self.size[1], self.border_radius)
-                    width : 1.2
-            on_release:
-                root.manager.current = "1"
-                root.manager.stop()
-        Label:
-            text: str(root.manager.time.time)
-            pos : 170, 460
-            size_hint : .1, .1
-            font_size : '45sp'
-        AsyncImage:
-            source: 'C:/Users/79263/Keep Fit/' + str(root.manager.num.num) + '.gif'
-            anim_delay: 0.5
-            
-        
-
-''')
-
+with open("design.kv", encoding='utf8') as design:
+    Builder.load_string(design.read())
+  
 class MyApp(App):
     def build(self):
         self.title = "Keep Fit"
@@ -133,5 +56,4 @@ class MyApp(App):
 
 
 if __name__ == '__main__':
-    
     MyApp().run()
